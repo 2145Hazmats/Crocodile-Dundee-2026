8b49461d8ce5505aea4c0b0fff3de8116c10fdd4
2145Hazmats <hazmatsocial@gmail.com>
2026-02-14 12:18:23 -0500

    Added Intake subsystem & assigned controls a-b

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,14 +4,27 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
+  private TalonFX intakeLeft;
+  private TalonFX intakeRight;
   /** Creates a new IntakeSubsystem. */
-  public IntakeSubsystem() {}
+  public IntakeSubsystem() {
+    intakeLeft = new TalonFX(IntakeConstants.LEFT_INTAKE_MOTOR_ID);
+    intakeRight = new TalonFX(IntakeConstants.RIGHT_INTAKE_MOTOR_ID);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void setIntakeMotors(double speed){
+    intakeLeft.set(speed);
+    intakeRight.set(speed);
   }
 }
